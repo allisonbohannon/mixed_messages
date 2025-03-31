53208fe09fa043a910e94ea5ed96a4798b93565e
Allison Bohannon <akboh.github@gmail.com>
2025-03-31 14:48:55 -0700

    Add message

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const celestialObjects = ['THe moon', 'Pluto', 'Mercury', 'Venus', 'Neputune']; 
+const celestialObjects = ['The moon', 'Pluto', 'Mercury', 'Venus', 'Neputune']; 
 const zodiacSigns = ['Aries', 'Aquarius', 'Virgo', 'Capricorn', 'Cancer', 'Leo', 'Libra', 'Saggittarius', 'Scorpio', 'Gemini', 'Pisces']; 
-const horoscopes = ['You will meet someone intereesting today', 'Avoid seafood', 'Buy a lotto ticket']; 
+const horoscopes = ['You will meet someone interesting today', 'Avoid seafood', 'Buy a lotto ticket']; 
 
 const messageGenerator = () => {
     const rand1 = Math.floor(Math.random() * celestialObjects.length); 
     const rand2 = Math.floor(Math.random() * zodiacSigns.length); 
     const rand3 = Math.floor(Math.random() * horoscopes.length); 
-    console.log(`rand1, rand2, rand3`)
+    console.log(`${celestialObjects[rand1]} is in ${zodiacSigns[rand2]}. ${horoscopes[rand3]}`)
 }
 
 messageGenerator(); 
