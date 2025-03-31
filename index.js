9497cfa20de6d739efa5306ccf97fad201c781c0
Allison Bohannon <akboh.github@gmail.com>
2025-03-31 14:46:32 -0700

    Add content to message arrays

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
-const celestialObjects = ['test1', 'test2', 'test3']; 
-const zodiacSigns = ['test1', 'test2', 'test3']; 
-const horoscopes = ['test1', 'test2', 'test3']; 
+const celestialObjects = ['THe moon', 'Pluto', 'Mercury', 'Venus', 'Neputune']; 
+const zodiacSigns = ['Aries', 'Aquarius', 'Virgo', 'Capricorn', 'Cancer', 'Leo', 'Libra', 'Saggittarius', 'Scorpio', 'Gemini', 'Pisces']; 
+const horoscopes = ['You will meet someone intereesting today', 'Avoid seafood', 'Buy a lotto ticket']; 
 
 const messageGenerator = () => {
     const rand1 = Math.floor(Math.random() * celestialObjects.length); 
     const rand2 = Math.floor(Math.random() * zodiacSigns.length); 
     const rand3 = Math.floor(Math.random() * horoscopes.length); 
-    console.log(rand1, rand2, rand3)
+    console.log(`rand1, rand2, rand3`)
 }
 
 messageGenerator(); 
