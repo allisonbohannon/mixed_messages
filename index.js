83fcf03758eb2aa3a10778da42a6273596c99990
Allison Bohannon <akboh.github@gmail.com>
2025-03-31 14:42:25 -0700

    Add random number generators

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log("Hello, world")
+const celestialObjects = ['test1', 'test2', 'test3']; 
+const zodiacSigns = ['test1', 'test2', 'test3']; 
+const horoscopes = ['test1', 'test2', 'test3']; 
+
+const messageGenerator = () => {
+    const rand1 = Math.floor(Math.random() * celestialObjects.length); 
+    const rand2 = Math.floor(Math.random() * zodiacSigns.length); 
+    const rand3 = Math.floor(Math.random() * horoscopes.length); 
+    console.log(rand1, rand2, rand3)
+}
+
+messageGenerator(); 
+
